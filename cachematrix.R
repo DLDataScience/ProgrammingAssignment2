9a1c9b081c0abc325c0868861cea645e34c13019
DLDataScience <dlevy93@gmail.com>
2020-10-27 20:59:21 -0700

    Code written, not yet tested.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below assume that they are dealing with an invertible matrix.
+## The purpose of these functions is to cache the inverse of some matrix, x, and to retrieve it
+## if the calculation has already been cached or to execute it if the calculation has not yet been cached.
 
-## Write a short comment describing this function
+
+## Creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+   
+   # initiate inv with a default NULL value
+   inv <- NULL
+   
+   # initiate `set()` as a function to reassign x using <<- and R scoping rules
+   set <- function(y) {
+      x <<- y
+      inv <- NULL
+   }
+   
+   # initiate `get()` as a function to retrieve the current value of x
+   get <- function() x
+   
+   # initiate `setinv()` as a function to reassign inv using <<- and R scoping rules
+   setinv <- function(i) inv <<- i
+   
+   # initiate `getinv()` as a function to retrieve the current inverse of x
+   getinv <- function() inv
+   
+   return(list(set = set, get = get,
+               setinv = setinv, getinv = getinv))
 
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then 
+## cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   inv <- x$getinv()
+   
+   if(!is.null(inv)) {
+      message('getting cached data')
+      return(inv)
+   }
+   
+   mat <- x$get
+   inv <- solve(mat)
+   x$setinv(inv)
+   
+   return(inv)
 }
